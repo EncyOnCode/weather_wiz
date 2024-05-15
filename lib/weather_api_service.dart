25ca7817e460f23dc0a5ffa8617bd20fa2d9b1fb
Enclosed <132351178+EncyOnCode@users.noreply.github.com>
2024-05-15 14:07:00 +0400

    updated .gitignore

--- lib/weather_api_service.dart
+++ lib/weather_api_service.dart
@@ -1,9 +1,10 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:weather_project/api_key.dart';
 
 class WeatherApi {
   static const String baseUrl = 'https://api.weatherapi.com/v1';
-  static const String apiKey = 'cd08a62987c1474481753333241505';
+  static const String apiKey = yourApiKey;
 
   Future<Map<String, Object?>> fetchWeather(String city) async {
     var url = '$baseUrl/current.json?key=$apiKey&q=$city';
